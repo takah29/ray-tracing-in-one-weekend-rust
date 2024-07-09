f62fe31fec0959e7147e862df206f48dfe82d4f6
takah29 <takah0209@gmail.com>
2024-07-10 02:16:39 +0900

    複数のサンプルを使ったピクセルの描画

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use ray_tracing_in_one_weekend::{
+    camera::Camera,
+    color,
     hittable::{HitRecord, Hittable},
     hittable_list::HittableList,
+    point3,
     ray::Ray,
-    rtweekend::INFINITY,
+    rtweekend::{random, Color, Point3, INFINITY},
     sphere::Sphere,
     utils::write_color,
-    vec3::{Color, Point3, Vec3},
-    {color, point3, vec3},
 };
 
 fn ray_color(r: Ray, world: &HittableList) -> Color {
@@ -16,7 +17,7 @@ fn ray_color(r: Ray, world: &HittableList) -> Color {
     }
 
     let unit_direction = r.dir.unit();
-    let t = 0.5 * (unit_direction.y + 1.0);
+    let t = 0.5 * (unit_direction.e[1] + 1.0);
     (1.0 - t) * color!(1, 1, 1) + t * color!(0.5, 0.7, 1.0)
 }
 
@@ -24,33 +25,28 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 384;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     println!("P3\n{} {}\n255", image_width, image_height);
-
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = point3!(0, 0, 0);
-    let horizontal = vec3!(viewport_width, 0, 0);
-    let vertical = vec3!(0, viewport_height, 0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - vec3!(0, 0, focal_length);
 
     let mut world = HittableList::new();
     world.add(Box::new(Sphere::new(point3!(0, 0, -1), 0.5)));
     world.add(Box::new(Sphere::new(point3!(0, -100.5, -1), 100.0)));
 
+    let cam = Camera::new();
+
     for j in (0..image_height).rev() {
         eprint!("\rScanline remaining: {:3}", j);
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
-            let pixel_color = ray_color(r, &world);
-            write_color(pixel_color);
+            let mut pixel_color = color!(0, 0, 0);
+            for _ in 0..samples_per_pixel {
+                let u = (i as f64 + random()) / (image_width - 1) as f64;
+                let v = (j as f64 + random()) / (image_height - 1) as f64;
+                let r = cam.get_ray(u, v);
+
+                pixel_color += ray_color(r, &world);
+            }
+            write_color(pixel_color, samples_per_pixel);
         }
     }
     eprintln!("\nDone");
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,9 @@
 use crate::vec3::Color;
 
-pub fn write_color(pixel_color: Color) {
-    let val = 255.999 * pixel_color;
-    let ir = val.x as i32;
-    let ig = val.y as i32;
-    let ib = val.z as i32;
+pub fn write_color(pixel_color: Color, samples_per_pixel: i32) {
+    let [ir, ig, ib] = (pixel_color / samples_per_pixel as f64)
+        .e
+        .map(|x| (256.0 * x.clamp(0.0, 0.999)) as u8);
 
     println!("{} {} {}", ir, ig, ib);
 }
