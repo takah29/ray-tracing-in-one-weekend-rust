aa70ae07a328b914f4fd0ca15494f1007718bbb7
takah29 <takah0209@gmail.com>
2024-07-10 03:14:56 +0900

    シャドウアクネを消す

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn ray_color(r: Ray, world: &HittableList, depth: i32) -> Color {
         return color!(0, 0, 0);
     }
 
-    if world.hit(&r, 0.0, INFINITY, &mut rec) {
+    if world.hit(&r, 0.001, INFINITY, &mut rec) {
         let target = rec.p + rec.normal + random_in_unit_sphere();
         return 0.5 * ray_color(Ray::new(rec.p, target - rec.p), world, depth - 1);
     }
