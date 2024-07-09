9b465d030b8a010e5dc9a691ce93e5db76bcc61c
takah29 <takah0209@gmail.com>
2024-07-10 03:12:40 +0900

    ガンマ補正による明るさの調整

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
 use crate::vec3::Color;
 
 pub fn write_color(pixel_color: Color, samples_per_pixel: i32) {
+    // ガンマ補正のために平方根を取っている
     let [ir, ig, ib] = (pixel_color / samples_per_pixel as f64)
         .e
-        .map(|x| (256.0 * x.clamp(0.0, 0.999)) as u8);
+        .map(|x| (256.0 * x.sqrt().clamp(0.0, 0.999)) as u8);
 
     println!("{} {} {}", ir, ig, ib);
 }
