e0f363d5924f5b71e619fa38c233a685b09b4ea4
takah29 <takah0209@gmail.com>
2024-07-12 04:29:03 +0900

    ランバーティアンの実装

--- src/material.rs
+++ src/material.rs
@@ -1,12 +1,40 @@
 use crate::hittable::HitRecord;
 use crate::rtweekend::{Color, Ray};
+use crate::vec3::random_unit_vector;
 
 pub trait Material {
     fn scatter(
         &self,
-        ray: &Ray,
+        r_in: &Ray,
         rec: &HitRecord,
         attenuation: &mut Color,
         scattered: &mut Ray,
     ) -> bool;
 }
+
+pub struct Lambertian {
+    albedo: Color,
+}
+
+impl Lambertian {
+    pub fn new(albedo: &Color) -> Self {
+        Self { albedo: *albedo }
+    }
+}
+
+impl Material for Lambertian {
+    fn scatter(
+        &self,
+        _: &Ray,
+        rec: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+    ) -> bool {
+        let scatter_direction = rec.normal + random_unit_vector();
+        *scattered = Ray::new(rec.p, scatter_direction);
+        *attenuation = self.albedo;
+        true
+    }
+}
+
+pub struct Metal {}
