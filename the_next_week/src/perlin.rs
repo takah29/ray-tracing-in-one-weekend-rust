4ced0cbfb4a339708cf354b829ad87c9d9a2c40b
takah29 <takah0209@gmail.com>
2024-07-18 01:18:16 +0900

    乱流ノイズ

--- the_next_week/src/perlin.rs
+++ the_next_week/src/perlin.rs
@@ -51,6 +51,21 @@ impl Perlin {
 
         perlin_interp(&c, u, v, w)
     }
+
+    pub fn turb(&self, p: &Point3, depth: i32) -> f64 {
+        let mut accum = 0.0;
+        let mut temp_p = *p;
+        let mut weight = 1.0;
+
+        for _ in 0..depth {
+            accum += weight * self.noise(&temp_p);
+            weight *= 0.5;
+            temp_p *= 2.0;
+        }
+
+        accum.abs()
+    }
+
     fn perlin_generate_perm() -> Vec<i32> {
         let mut p = (0..Self::POINT_COUNT as i32).collect();
         Self::permute(&mut p);
