0c56f5af7338208ee00b40887128a39230335027
takah29 <takah0209@gmail.com>
2024-07-18 01:21:14 +0900

    位相の調整

--- the_next_week/src/texture.rs
+++ the_next_week/src/texture.rs
@@ -60,6 +60,6 @@ impl NoiseTexture {
 
 impl Texture for NoiseTexture {
     fn value(&self, _: f64, _: f64, p: &Point3) -> Color {
-        color!(1, 1, 1) * self.noise.turb(&(self.scale * *p), 7)
+        color!(1, 1, 1) * 0.5 * (1.0 + (self.scale * p.e[2] + 10.0 * self.noise.turb(p, 7)).sin())
     }
 }
