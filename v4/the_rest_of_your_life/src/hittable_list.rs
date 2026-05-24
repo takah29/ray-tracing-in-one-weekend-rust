eb65f85b66639f6d437122c03d667722568f1393
takah29 <takah0209@gmail.com>
2026-05-24 17:10:07 +0900

    オブジェクト作成を削除

--- v4/the_rest_of_your_life/src/hittable_list.rs
+++ v4/the_rest_of_your_life/src/hittable_list.rs
@@ -38,15 +38,13 @@ impl HittableList {
 
 impl Hittable for HittableList {
     fn hit(&self, r: &Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
-        let mut temp_rec = HitRecord::default();
         let mut hit_anything = false;
         let mut closest_so_far = ray_t.max;
 
         for object in &self.objects {
-            if object.hit(r, Interval::new(ray_t.min, closest_so_far), &mut temp_rec) {
+            if object.hit(r, Interval::new(ray_t.min, closest_so_far), rec) {
                 hit_anything = true;
-                closest_so_far = temp_rec.t;
-                *rec = temp_rec.clone();
+                closest_so_far = rec.t;
             }
         }
         hit_anything
